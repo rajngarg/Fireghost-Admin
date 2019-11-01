36fbeab529f9ceeace3e41af6cd49cf4f93e2bbc
Rajan Garg <dev.rajangarg@gmail.com>
2019-11-01 22:52:59 +0530

    added epoch converter

--- src/app/screens/pages/pages.component.ts
+++ src/app/screens/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../../api/api.service';
 import {PostModel} from '../posts/posts.component';
+import timeago from 'epoch-timeago';
 
 @Component({
   selector: 'app-pages',
@@ -24,6 +25,9 @@ export class PagesComponent implements OnInit {
   displayedColumns: string[] = ['title', 'created', 'slug'];
 
   setData(res: Array<PostModel>) {
+    for (let i = 0; i < res.length; i++) {
+      res[i].created = timeago(Number(res[i].created) * 1000);
+    }
     this.dataSource = res;
   }
 
--- src/app/screens/posts/posts.component.ts
+++ src/app/screens/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../../api/api.service';
+import timeago from 'epoch-timeago';
 
 export interface PostModel {
   body: string;
@@ -20,8 +21,8 @@ export class PostsComponent implements OnInit {
   constructor(private api: ApiService) {
     api.getPosts(false)
       .then((res: Array<PostModel>) => {
+        console.log(res);
         this.setData(res);
-        console.log(res[0]);
       }).catch(err => {
       console.log(err);
     });
@@ -32,6 +33,9 @@ export class PostsComponent implements OnInit {
   displayedColumns: string[] = ['title', 'created', 'slug'];
 
   setData(res: Array<PostModel>) {
+    for (let i = 0; i < res.length; i++) {
+      res[i].created = timeago(Number(res[i].created) * 1000);
+    }
     this.dataSource = res;
   }
 
